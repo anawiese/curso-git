f4c50368630128d53718307ca9e6f186d1e5a61b
Ana Wiese <ana.wieset@gmail.com>
2019-09-25 18:55:33 -0300

    Projeto iniciado

---
+++ App Git/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace App_Git
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
